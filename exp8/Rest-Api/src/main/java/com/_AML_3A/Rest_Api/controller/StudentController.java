329e96087d735db94f55384430062e964eb22426
Rakshit Jaryal <rakshjaryal@gmail.com>
2026-03-06 15:31:24 +0530

    new change

---
+++ exp8/Rest-Api/src/main/java/com/_AML_3A/Rest_Api/controller/StudentController.java
@@ -0,0 +1,27 @@
+package com._AML_3A.Rest_Api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com._AML_3A.Rest_Api.model.Student;
+import com._AML_3A.Rest_Api.service.StudentService;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return service.getAllStudents();
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+}
---
+++ exp8/Rest-Api/src/main/java/com/_AML_3A/Rest_Api/service/StudentService.java
@@ -0,0 +1,29 @@
+package com._AML_3A.Rest_Api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com._AML_3A.Rest_Api.model.Student;
+import com._AML_3A.Rest_Api.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+	@Autowired
+    private StudentRepository repository;
+
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+
+    public Student saveStudent(Student student) {
+        return repository.save(student);
+    }
+    
+    @SuppressWarnings("deprecation")
+	public Student getStudentById(int id) {
+    	return repository.getById(id);
+    }
+}
